180bddbf5d4a29836fde2c15b1f976af210a630c
Victoria Akeyo <vickyakeke@gmail.com>
2022-01-14 11:43:31 +0300

    Initial Commit

---
+++ MoneySend/MoneySendApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneySendApp.swift
+//  MoneySend
+//
+//  Created by Victoria on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneySendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
